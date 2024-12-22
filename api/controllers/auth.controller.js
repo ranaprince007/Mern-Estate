110daa315b9bee1aaa360e2bbf54eb94ee4b3aff
Prince Kumar <princesinghsisodiya30@gmail.com>
2024-12-23 02:38:32 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from 'jsonwebtoken';
 export const signup= async (req,res,next)=>{
     const { username, email ,password}=req.body;
     console.log("data",req.body)
@@ -14,3 +14,20 @@ export const signup= async (req,res,next)=>{
         next(error);
     }   
 };
+export const signin=async (req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const ValidUser=await User.findOne({email});
+        if(!ValidUser) return next(errorHandler(404,'User not found'));
+        const validPassword=bcryptjs.compareSync(password,ValidUser.password);
+        if(!validPassword)return next(errorHandler(401,'Wrong Password'));
+        const token=jwt.sign({id:ValidUser._id},process.env.JWT_SECRET);
+        const {password:pass, ...rest}=ValidUser._doc;
+        res
+           .cookie('access_token',token,{httpOnly:true})
+           .status(200)
+           .json(ValidUser);
+    } catch (error) {
+        next(error);
+    }
+}
